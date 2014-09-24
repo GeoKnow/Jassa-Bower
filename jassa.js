bdd791cfab79cea4906576b391ba2e3de325b24a
cstadler <cstadler@informatik.uni-leipzig.de>
2014-09-24 17:31:56 +0200

    Updating version 0.0.4-SNAPSHOT with tag v0.0.4-SNAPSHOT

--- jassa.js
+++ jassa.js
@@ -24172,7 +24172,7 @@ ns.createDefaultConstraintElementFactories = function() {
                 lat: vy, // '?y'
                 wkt: function(b) {
                     var result = 'POINT(' + b.get(vx).getLiteralValue() + ' ' + b.get(vy).getLiteralValue() + ')';
-					//var result = rdf.NodeFactory.createTypedLiteralFromString(result, 'http://www.opengis.net/ont/geosparql#wktLiteral');
+					result = rdf.NodeFactory.createTypedLiteralFromString(result, 'http://www.opengis.net/ont/geosparql#wktLiteral');
 					return result;
 				}
             }],
